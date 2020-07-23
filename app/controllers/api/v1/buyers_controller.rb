a65cacbdb808c5064a4cd443929eadea77583cd4
Malcolm_Leake <echelon1089@gmail.com>
2020-07-22 22:36:48 -0400

    Creating Controllers

--- app/controllers/api/v1/buyers_controller.rb
+++ app/controllers/api/v1/buyers_controller.rb
@@ -1,2 +1,63 @@
 class Api::V1::BuyersController < ApplicationController
+    def index
+        @buyer = Buyer.all
+        serialized_data = PostSerializer.new(@buyer).serialized_json
+        render json: serialized_data, status: 200
+    end
+
+
+    def new
+        @buyer = Buyer.new
+
+        render json: @buyer
+    end
+
+    def show
+        @buyer = Buyer.find(params[:id])
+
+        render json: @buyer, status: 200
+    end
+
+    def create
+        @buyer = Buyer.new(buyer_params)
+        if  @buyer.save
+            serialized_data = PostSerializer.new(@buyer).serialized_json
+            
+        render json: serialized_data, status: 200
+        else
+        render 'new'
+        end
+    end
+
+    def show
+        @buyer = Buyer.find(params[:id])
+        render json: @buyer
+    end
+
+    def edit
+        @buyer = Buyer.find(params[:id])
+        render json: @buyer, status:200
+    end
+
+    def update
+        if @buyer.update(buyer_params)
+            render json: @buyer, status: 200
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @buyer = Buyer.find(params[:id])
+        @buyer.destroy
+        
+        render json: {postId: @buyer.id}
+    end
+
+    private
+
+    def buyer_params
+        params.require(:buyer).permit(:propertyType, :propertyValue, :timeFrame,:state, :city, :zipCode, :fullName, :phoneNumber    )
+    end
+
 end
--- app/controllers/api/v1/sellers_controller.rb
+++ app/controllers/api/v1/sellers_controller.rb
@@ -1,2 +1,64 @@
-class SellersController < ApplicationController
+class Api::V1::SellersController < ApplicationController
+    
+    def index
+        @seller = Seller.all
+        serialized_data = PostSerializer.new(@seller).serialized_json
+        render json: serialized_data, status: 200
+    end
+
+
+    def new
+        @seller = Seller.new
+
+        render json: @seller
+    end
+
+    def show
+        @seller = Seller.find(params[:id])
+
+        render json: @seller, status: 200
+    end
+
+    def create
+        @seller = Seller.new(seller_params)
+        if  @seller.save
+            serialized_data = PostSerializer.new(@seller).serialized_json
+            
+        render json: serialized_data, status: 200
+        else
+        render 'new'
+        end
+    end
+
+    def show
+        @seller = Seller.find(params[:id])
+        render json: @seller
+    end
+
+    def edit
+        @seller = Seller.find(params[:id])
+        render json: @seller, status:200
+    end
+
+    def update
+        if @seller.update(seller_params)
+            render json: @seller, status: 200
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @seller = Seller.find(params[:id])
+        @seller.destroy
+        
+        render json: {postId: @seller.id}
+    end
+
+    private
+
+    def seller_params
+        params.require(:seller).permit(:propertyType, :propertyValue, :timeFrame,:state, :city, :zipCode, :fullName, :phoneNumber    )
+    end
+
 end
